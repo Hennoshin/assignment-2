980f481cc6b8cff035f00d60ab39faa825d8592e
diditlion <permana.aditya97@gmail.com>
2023-05-05 11:07:50 +0700

    bugs fasilitas

--- resources/views/pages/fasilitas/list.blade.php
+++ resources/views/pages/fasilitas/list.blade.php
@@ -55,7 +55,7 @@
                                                 class="bx bx-edit-alt me-1"></i>
                                             Edit</a>
                                         <form id="form_delete_{{ $item->uuid }}"
-                                            action="{{ route('web.informations.delete', ['id' => $item->uuid]) }}"
+                                            action="{{ route('web.fasilitas.delete', ['id' => $item->uuid]) }}"
                                             method="POST">
                                             {{ csrf_field() }}
                                             {{ method_field('DELETE') }}
